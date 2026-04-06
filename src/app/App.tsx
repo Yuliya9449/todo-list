749e8c38d8889d8405778938b790e29ccd8efee6
Yuliya9449 <yulya9449@yandex.ru>
2026-04-06 17:50:28 +0300

    refactor: extract Main component from App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,11 @@
 import styles from './App.module.css'
-import { CreateItemForm } from '@/common/components/CreateItemForm/CreateItemForm'
-import { createTodolistAC } from '@/features/model/todolists-reducer'
-import { useAppDispatch, useAppSelector } from '@/common/hooks'
-import { Todolists } from '@/features/todolists/ui/Todolists/Todolists'
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
+import { useAppSelector } from '@/common/hooks'
 import { Header } from '@/common/components/Header/Header'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { selectThemeMode } from '@/app/model/app-selectors'
 import { getTheme } from '@/common/theme/theme'
+import { Main } from '@/app/Main'
 
 export type Todolist = {
   id: string
@@ -28,32 +24,15 @@ export type TasksState = Record<Todolist['id'], Task[]>
 export type FilterValues = 'all' | 'active' | 'completed'
 
 export const App = () => {
-  const dispatch = useAppDispatch()
-
   const themeMode = useAppSelector(selectThemeMode)
   const theme = getTheme(themeMode)
 
-  const createTodolistHandler = (title: Todolist['title']) => dispatch(createTodolistAC(title))
-
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <div className={styles.app}>
+        <CssBaseline />
         <Header />
-        <Container maxWidth={'lg'}>
-          <Grid
-            container
-            sx={{ p: '30px 0' }}
-          >
-            <CreateItemForm onCreateItem={createTodolistHandler} />
-          </Grid>
-          <Grid
-            container
-            spacing={4}
-          >
-            <Todolists />
-          </Grid>
-        </Container>
+        <Main />
       </div>
     </ThemeProvider>
   )
---
+++ src/app/Main.tsx
@@ -0,0 +1,30 @@
+import Grid from '@mui/material/Grid'
+import { CreateItemForm } from '@/common/components/CreateItemForm/CreateItemForm'
+import { Todolists } from '@/features/todolists/ui/Todolists/Todolists'
+import Container from '@mui/material/Container'
+import { createTodolistAC } from '@/features/model/todolists-reducer'
+import type { Todolist } from '@/app/App'
+import { useAppDispatch } from '@/common/hooks'
+
+export const Main = () => {
+  const dispatch = useAppDispatch()
+
+  const createTodolistHandler = (title: Todolist['title']) => dispatch(createTodolistAC(title))
+
+  return (
+    <Container maxWidth={'lg'}>
+      <Grid
+        container
+        sx={{ p: '30px 0' }}
+      >
+        <CreateItemForm onCreateItem={createTodolistHandler} />
+      </Grid>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Todolists />
+      </Grid>
+    </Container>
+  )
+}
