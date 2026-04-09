8942c3978dfbff40ee1c78ba904dce20909f89aa
Yuliya9449 <yulya9449@yandex.ru>
2026-04-09 18:22:37 +0300

    refactor: remove redundant combineReducers

--- src/app/model/store.ts
+++ src/app/model/store.ts
@@ -1,19 +1,15 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { tasksReducer, tasksSlice } from '@/features/todolists/model/tasks-slice'
 import { todolistsReducer, todolistsSlice } from '@/features/todolists/model/todolists-slice'
 import { appReducer, appSlice } from '@/app/model/app-slice'
 
-const rootReducer = combineReducers({
-  [tasksSlice.name]: tasksReducer,
-  [todolistsSlice.name]: todolistsReducer,
-  [appSlice.name]: appReducer,
-})
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    [tasksSlice.name]: tasksReducer,
+    [todolistsSlice.name]: todolistsReducer,
+    [appSlice.name]: appReducer,
+  },
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
